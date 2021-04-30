5a0193e3c7785011983d0debef3c28de100f97b8
umangU <72771903+umangU@users.noreply.github.com>
2021-05-01 09:22:03 +1000

    Code Updated

--- PCA.r
+++ PCA.r
@@ -18,30 +18,30 @@ variance <- apply(plants, 2, var)
 format(variance, scientific = F)
 # Performing PCA on the plants dataset
 pr.out=prcomp(plants_data, scale=TRUE)
-#Function to assign distinct color to each variable
+# Function to assign distinct color to each variable
 Cols=function (vec ){
  cols=rainbow (length (unique (vec )))
  return (cols[as.numeric (as.factor (vec))])
 }
-#Plotting the principal components
+# Plotting the principal components
 par(mfrow =c(1,2))
 plot(pr.out$x [,1:2], col=Cols(plants),pch =19, xlab ="Z1",ylab="Z2")
 plot(pr.out$x [,1:3], col=Cols(plants),pch =19, xlab ="Z1",ylab="Z3")
-#Getting the summary of the proportion of variance explained by principal component
+# Getting the summary of the proportion of variance explained by principal component
 summary(pr.out)
-#Plotting the variance explained by the first few principal components
+# Plotting the variance explained by the first few principal components
 par(mfrow =c(1,1))
 plot(pr.out)
-#Calculating the proportion of variance explained
+# Calculating the proportion of variance explained
 pve=100*pr.out$sdev^2/sum(pr.out$sdev ^2)
 
-#Plotting the PVE of each principal component
+# Plotting the PVE of each principal component
 par(mfrow =c(1,2))
 plot(pve, type ="o", ylab="PVE", xlab="Principal Component",col="blue")
 
-#Plotting the cummulative PVE of each principal component
+# Plotting the cummulative PVE of each principal component
 plot(cumsum(pve ), type="o", ylab ="Cumulative PVE", xlab="Principal Component", col="brown3")
 
-#Biplot
+# Biplot
 par(mfrow=c(1,1))
 biplot(pr.out, scale=TRUE)
