505d12265dd008a192f9f9002e7e6a6dcda57213
umangU <72771903+umangU@users.noreply.github.com>
2021-06-14 21:48:22 +1000

    Code Updated

--- PCA.r
+++ PCA.r
@@ -10,24 +10,24 @@ plants_data$Site=as.numeric(plants_data$Site)
 plants <- plants_data[,-1]
 # Calculate the means of the variable
 means <- apply(plants, 2, mean)
-#Format the value to remove the scientific notation
+# Format the value to remove the scientific notation
 format(means, scientific = F)
-#Calculate the variance of the variable
+# Calculate the variance of the variable
 variance <- apply(plants, 2, var)
-#Format the value to remove the scientific notation
+# Format the value to remove the scientific notation
 format(variance, scientific = F)
-#Performing PCA on the plants dataset
+# Performing PCA on the plants dataset
 pr.out=prcomp(plants_data, scale=TRUE)
-#Function to assign distinct color to each variable
+# Function to assign distinct color to each variable
 Cols=function (vec ){
  cols=rainbow (length (unique (vec )))
  return (cols[as.numeric (as.factor (vec))])
 }
-#Plotting the principal components
+# Plotting the principal components
 par(mfrow =c(1,2))
 plot(pr.out$x [,1:2], col=Cols(plants),pch =19, xlab ="Z1",ylab="Z2")
 plot(pr.out$x [,1:3], col=Cols(plants),pch =19, xlab ="Z1",ylab="Z3")
-#Getting the summary of the proportion of variance explained by principal component
+# Getting the summary of the proportion of variance explained by principal component
 summary(pr.out)
 #Plotting the variance explained by the first few principal components
 par(mfrow =c(1,1))
