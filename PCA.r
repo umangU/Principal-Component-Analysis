48c5d980bd06d6c8e716937ea5cf58d175a6911d
umangU <72771903+umangU@users.noreply.github.com>
2021-07-18 23:43:28 +1000

    Code Updated

--- PCA.r
+++ PCA.r
@@ -1,6 +1,6 @@
-#Reading the plants dataset
+# Reading the plants dataset
 plants_data <- read_xlsx(file.choose())
-#Get the row names of the dataset
+# Get the row names of the dataset
 plants = row.names(plants_data)
 #Get the variable names of the dataset
 names(plants_data)
