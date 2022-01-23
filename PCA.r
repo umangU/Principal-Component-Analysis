fc3cb4d59bde0e1c47b58e72541dbfccdf21e563
umangU <72771903+umangU@users.noreply.github.com>
2022-01-23 21:15:46 +1100

    Code Updated

--- PCA.r
+++ PCA.r
@@ -3,9 +3,9 @@ plants_data <- read_xlsx(file.choose())
 # Get the row names of the dataset
 plants = row.names(plants_data)
 names(plants_data)
-#Convert the site column from character to numeric
+# Convert the site column from character to numeric
 plants_data$Site=as.numeric(plants_data$Site)
-#Remove the site column from the dataset while keeping only the quantitive variable
+# Remove the site column from the dataset while keeping only the quantitive variable
 plants <- plants_data[,-1]
 # Calculate the means of the variable
 means <- apply(plants, 2, mean)
