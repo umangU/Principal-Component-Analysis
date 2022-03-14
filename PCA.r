f65be613741767f22b22b0dfbb1213de4a8af11c
umangU <72771903+umangU@users.noreply.github.com>
2022-03-14 23:59:03 +1100

    Code Updated

--- PCA.r
+++ PCA.r
@@ -3,7 +3,7 @@ plants_data <- read_xlsx(file.choose())
 # Get the row names of the dataset
 plants = row.names(plants_data)
 names(plants_data)
-#Convert the site column from character to numeric
+# Convert the site column from character to numeric
 plants_data$Site=as.numeric(plants_data$Site)
 # Remove the site column from the dataset while keeping only the quantitive variable
 plants <- plants_data[,-1]
